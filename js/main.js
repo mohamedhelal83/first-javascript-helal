0e076205413416ee545febcca4c38d256c7cd196
mohamed helal <147198125+mohamedhelal83@users.noreply.github.com>
2024-03-20 21:02:55 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+//star
+alert("hello");
+document.getElementById("helal").innerHTML="mohamed helal";
+document.getElementById("mohamed").style.color="blue";
+document.getElementById("mohamed").style.textAlign="center";
